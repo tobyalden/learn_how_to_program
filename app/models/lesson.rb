f26dfbafca23554cdb3a0bfb0b79a2fe8a1b2fb6
Toby Alden & Ben Cornelis <student@epicodus.com>
2015-06-30 12:06:25 -0700

    Refactored lesson#next to use database commands.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,12 +3,15 @@ class Lesson < ActiveRecord::Base
   validates :content, :presence => true
 
   def next
-    all = Lesson.all
-    all = all.sort_by {|lesson| lesson.number}
-    binding.pry
-    if all.index(self) < all.length - 1
-      return all[all.index(self) + 1]
-    end
+    return Lesson.where("number > #{number}").order("number").first
   end
+
+  # def next
+  #   all = Lesson.all
+  #   all = all.sort_by {|lesson| lesson.number}
+  #   if all.index(self) < all.length - 1
+  #     return all[all.index(self) + 1]
+  #   end
+  # end
 
 end
